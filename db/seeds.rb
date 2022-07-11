a9c05323735654b5e377745fb10514b33a70e20d
Gina Mirando <gina.mirando5@gmail.com>
2022-07-11 12:52:50 -0500

    updated products

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,19 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+product = Product.create({name: "Fern 'Birds Nest Hurricane'", price: 30, description: "The Asplenium antiquum Hurricane Bird's nest fern is a relatively new variety that has a wonderful whirlwind growth habit of glossy green leaves. It prefers a degree of humidity making it ideal for adding a touch of easy-care, air-purifying greenery to kitchens and bathrooms", image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/image_png_1578146101_974x1299.png?v=1657256639"})
+ 
+
+product = Product.create({name: "Croton 'Freckles'", price: 38, description: "The colorful foliage for this variety of croton, has spots similar to freckles as it is found all over the plant including leaves and stems. Leaves start off green with light yellow spots and eventually, leaves turn bright red with light red spots", image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/6_CROTON_FRECKLES_3_974x974.jpg?v=1654050614" })
+
+product = Product.create({name: "ZZ Zamioculcas 'Black Raven'", price: 70, description: "Zamioculcas raven will survive in very low light conditions. That’s why it is suitable for use in the office or even in poorly lit rooms like bathrooms. However, for it to thrive and grow, put it in a very well-lit place while avoiding direct sunlight. Time is what makes the black ZZ plant turn dark. Fresh leaves start out bright green and gradually turn black with time", image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/6_ZZ_BLACK_RAVEN_3_de04daf2-121a-40ba-a7a3-b2cec7faa211_974x974.jpg?v=1654050727"})
+
+
+product = Product.create({name: "Ficus 'Umbellata'", price: 40, description: "Ficus umbellata is known for its wide, unique heart-shaped leaves and also thin stems. This type of plant makes an excellent indoor specimen because it can grow well in medium-high light conditions and be pruned into different shapes if desired. It’s easy to care for and offers a lot of variety in color and leaf shapes", image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/4_FICUS_UMBELLATA_3_3d22418b-c308-4a15-87f1-84b05dd6f692_421x421.jpg?v=1654050430"})
+
+
+product = Product.create({name: "Carnivorous 'Monkey Cup' Mini Basket", price: 30, description: "From its tendrils, you'll see globe- or tube-shaped protrusions that act as traps. In the tropics, monkeys are often seen drinking rainwater from these “pitchers” which is how it got its most popular nicknames", image_url:"https://cdn.shopify.com/s/files/1/2528/3612/products/CARNIVOROUS_MONKEY.CUP_MINI_BASKET_3_974x974.jpg?v=1655277522"})
+
+product = Product.create({name: "Schefflera 'Variegated'", price: 20, description: "The Umbrella plant is the dwarf version of the larger schefflera acrinophylla native to Taiwan. This evergreen wields very slender stalks with oblong leaves arranged in a circular fashion at the end of each stem. This particular variegated variety will have splashes of washed out yellow on many of its leaves" , image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/8-VARIGATEDSHEFLERA-3_421x421.jpg?v=1630011867"})
+
+
+product = Product.create({name: "Begonia 'Pink Spotted'", price: 30, description: "Begonia Amphioxus is a small but eye-catching plant that you can never deny introducing in your garden. The beautiful plant does not bear negligence, so you need to take care of it and provide it with the right growing conditions. Begonia Amphioxus is native to Borneo, Indonesia, and is native to tropical regions. You will love to grow this plant in your household. The plant is bushy and belongs to the Begonia family. The plant’s scientific name originates from ‘amphi’ ‘oxys’, meaning that the plant has pointed ends and they are elongated as well", image_url: "https://cdn.shopify.com/s/files/1/2528/3612/products/4_BEGONIA_PINK_SPOTTED_3_300228fa-978f-4b5f-b304-12d34eb68a7c_421x421.jpg?v=1654050318"})
+
+puts "Seeded Database"
