6d6f5875ad06978eb0b9fb83d407f0dcd3ae855e
Gina Mirando <gina.mirando5@gmail.com>
2022-07-11 15:04:42 -0500

    modified controller and routes

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -9,4 +9,15 @@ class ProductsController < ApplicationController
     product = Product.find_by(id: product_id) 
     render json: product.as_json
   end 
+
+  def create
+    product = Product.new(
+      name: params[:name],
+      price: params[:price],
+      description: params[:description],
+      image_url: params[:image_url]
+    )
+    product.save
+    render json: product.as_json
+  end 
 end 
