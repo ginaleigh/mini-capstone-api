a65b5f28105e83b4fed5aa419d8dd78fce6c741f
Gina Mirando <gina.mirando5@gmail.com>
2022-07-11 14:33:05 -0500

    restful routes for index and show

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,47 +1,12 @@
 class ProductsController < ApplicationController
-  def one_product_method
+  def index
+    products = Product.all
+    render json: products.as_json
+  end 
+
+  def show
     product_id = params["id"]
     product = Product.find_by(id: product_id) 
     render json: product.as_json
   end 
-  # def all_products 
-  #   products = Product.all
-  #   render json: products.as_json
-  # end 
-
-  # def first_product
-  #   product = Product.first 
-  #   render json: product.as_json
-  # end 
-
-  # def second_product
-  #   product = Product.second 
-  #   render json: product.as_json
-  # end 
-
-  # def third_product
-  #   product = Product.third 
-  #   render json: product.as_json
-  # end 
-
-  # def fourth_product
-  #   product = Product.fourth 
-  #   render json: product.as_json
-  # end 
-
-  # def fifth_product
-  #   product = Product.fifth 
-  #   render json: product.as_json
-  # end 
-
-  # def sixth_product
-  #   product = Product.sixth 
-  #   render json: product.as_json
-  # end 
-
-  # def seventh_product
-  #   product = Product.seventh 
-  #   render json: product.as_json
-  # end 
-
-end
+end 
--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,4 @@
 Rails.application.routes.draw do
-
-  get "/one_product/:id" => "products#one_product_method"
-
-  #get "/all_products", controller: "products", action: "all_products"
-
-  # verb path             controller#action
-  # get "all_products" => "products#all_products" 
-
-  # get "/first_product" => "products#first_product"
-  # get "/second_product" => "products#second_product"
-  # get "/third_product" => "products#third_product"
-  # get "/fourth_product" => "products#fourth_product"
-  # get "/fifth_product" => "products#fifth_product"
-  # get "/sixth_product" => "products#sixth_product"
-  # get "/seventh_product" => "products#seventh_product"
-
-end
+  get "/products" => "products#index"
+  get "/products/:id" => "products#show"
+end 
